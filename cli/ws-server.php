21a2489398a8fe70698e62de43e6806f2c8444a5
programarivm <info@programarivm.com>
2021-07-24 21:00:49 +0200

    Secured WebSocket server

--- cli/ws-server.php
+++ cli/ws-server.php
@@ -6,16 +6,29 @@ use ChessServer\Socket;
 use Ratchet\Server\IoServer;
 use Ratchet\Http\HttpServer;
 use Ratchet\WebSocket\WsServer;
+use React\Socket\Server;
+use React\Socket\SecureServer;
 
 require __DIR__  . '/../vendor/autoload.php';
 
-$server = IoServer::factory(
+$loop = \React\EventLoop\Factory::create();
+
+$socket = new Server('8443', $loop);
+$socket = new SecureServer($socket, $loop, [
+    'local_cert'  => __DIR__  . '/../ssl/cert.pem',
+    'local_pk' => __DIR__  . '/../ssl/key.pem',
+    'allow_self_signed' => true,
+    'verify_peer' => false
+]);
+
+$server = new IoServer(
     new HttpServer(
         new WsServer(
             new Socket()
         )
     ),
-    8080
+    $socket,
+    $loop
 );
 
 $server->run();
