74e6481801a2814fccfa7c76ad6532fc40b9d0fd
programarivm <info@programarivm.com>
2021-07-26 10:02:11 +0200

    Added cli/wss-server.php

--- cli/ws-server.php
+++ cli/ws-server.php
@@ -3,33 +3,19 @@
 namespace ChessServer;
 
 use ChessServer\Socket;
-use Ratchet\Http\HttpServer;
 use Ratchet\Server\IoServer;
+use Ratchet\Http\HttpServer;
 use Ratchet\WebSocket\WsServer;
-use React\EventLoop\Factory;
-use React\Socket\Server;
-use React\Socket\SecureServer;
 
 require __DIR__  . '/../vendor/autoload.php';
 
-$loop = Factory::create();
-
-$server = new Server('0.0.0.0:8443', $loop);
-
-$secureServer = new SecureServer($server, $loop, [
-    'local_cert'  => __DIR__  . '/../ssl/programarivm.com.crt',
-    'local_pk' => __DIR__  . '/../ssl/programarivm.com.key',
-    'allow_self_signed' => true,
-    'verify_peer' => false,
-    'verify_peer_name' => false
-]);
-
-$httpServer = new HttpServer(
-    new WsServer(
-        new Socket()
-    )
+$server = IoServer::factory(
+    new HttpServer(
+        new WsServer(
+            new Socket()
+        )
+    ),
+    8080
 );
 
-$ioServer = new IoServer($httpServer, $secureServer, $loop);
-
-$ioServer->run();
+$server->run();
