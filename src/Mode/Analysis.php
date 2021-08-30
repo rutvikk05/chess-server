c0886caf3675a6d5d1d3fa3dec7b8de054b54f34
programarivm <info@programarivm.com>
2021-08-30 10:21:06 +0200

    Cleaned up the modes

--- src/Mode/Analysis.php
+++ src/Mode/Analysis.php
@@ -2,8 +2,6 @@
 
 namespace ChessServer\Mode;
 
-use ChessServer\Command\PlayFen;
-
 class Analysis extends AbstractMode
 {
     const NAME = 'analysis';
--- src/Mode/PlayFriend.php
+++ src/Mode/PlayFriend.php
@@ -2,8 +2,6 @@
 
 namespace ChessServer\Mode;
 
-use ChessServer\Command\PlayFen;
-
 class PlayFriend extends AbstractMode
 {
     const NAME = 'playfriend';
